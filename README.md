7a5c2275d3959af532e208fcbf84b90f2c5505bc
fk-ln <fuga.kimu@gmailcom>
2021-05-21 12:00:37 +0900

    完成 README作成

---
+++ README.md
@@ -0,0 +1,29 @@
+#Numelody
+CSVから取得したデータをいい感じで補正した後音階に当てはめてMIDIファイルに出力する。
+Numerical  Melody的な感じで適当に付けてたけどそれっぽいのでそのまま採用
+
+##使い方
+numelody.pyをダウンロードして任意のフォルダに置く。
+そのディレクトリにCSVファイルを作成し1列目に順に数値を入力して保存。正の数であれば上限も無いし小数でも大丈夫...なはず。
+行名も列名も要らないです。
+以下の通り実行するとmelody.midが作られます。
+```python
+python numelody.py (CSVファイル名)
+```
+###例
+```
+python numelody.py sample
+```
+リポジトリにあるsample.csvは東京都の新規感染者数の推移です。お試しにどうぞ。
+
+##必要要件
+-python 3.6.10
+-pretty_midi 0.2.8
+-pands 1.0.5
+
+##その他
+Windowsだとダブルクリックで普通にMIDIファイルが再生可能なはず。Macだと大変？
+オンラインでMIDIからmp3，wavに変換するのあるのでその辺を使うのもありかも。
+
+##作者
+[fk-ln](https://github.com/fk-ln)
--- numelody.py
+++ numelody.py
@@ -1,17 +1,21 @@
 import pretty_midi
 import pandas as pd
+import sys
 
-df = pd.read_csv('note_numbers.csv', index_col=False,header=None)
-note_numbers=list(df[0])
+#読み込むファイル名は適宜書き換えてください
+df = pd.read_csv(sys.argv[1]+'.csv', index_col=False,header=None)
+note_numbers=list(df[0]) #1列目だけ取り出してリスト化
 
-#リストの中の最高音，最低音，サイズを取得
+#リストの中の最大値(最高音)，最小値(最低音)，サイズを取得
 h=max(note_numbers)
 l=min(note_numbers)
 length=len(note_numbers)
 
+#補正後の最高音，最低音を定義
 h0=108
 l0=36
 
+#定義した最高/低音に収まるようにリスト全体に補正をかける
 adjusted_note_numbers=[int((n-l)*(h0-l0)/(h-l)+l0) for n in note_numbers]
 print(adjusted_note_numbers)
 print(max(adjusted_note_numbers))
@@ -19,40 +23,30 @@ print(min(adjusted_note_numbers))
 
 # Pretty MIDIオブジェクトを作成
 melody = pretty_midi.PrettyMIDI()
-
 # Acostic Grand Pianoに対応するプログラム番号を返す
 piano_program = pretty_midi.instrument_name_to_program('Acoustic Grand Piano')
 #Instrument Instanceを作成
 piano = pretty_midi.Instrument(program=piano_program)
 
-#取得したnote_numberのlistを表示
-#print(note_numbers)
+count=0
 
-
-
-
-
-count=0.0
 for note_number in adjusted_note_numbers:
 
     #"C5"などのnote_nameを入れると対応するnote_numberを返す。今回は使わないけど一応残しておく。
     #note_number = pretty_midi.note_name_to_number(note_name)
 
     # NoteInstanceを作成。音(pitch)の開始時間と終了時間、velocityを定義
+    #start,endにかける値[s]が1音あたりの長さ[s]
     note = pretty_midi.Note(
         velocity=100, pitch=note_number, start=count*0.1, end=(count+1)*0.1)
 
-    # Add it to our piano instrument
-    # 上記で作成したNoteInstanceをPiano Instrumentに加えます。
+    # NoteInstanceをPiano Instrumentにappend
     piano.notes.append(note)
 
     count=count+1
 
-
-# Piano InstrumentをPretty MIDIオブジェクトにAppend
+# Piano InstrumentをPretty MIDIオブジェクトにappend
 melody.instruments.append(piano)
 
-
-# Write out the MIDI data
-# PrettyMIDIオブジェクトをMIDIファイルとして書き出しましょう。
+# MIDIファイルとして書き出し
 melody.write('melody.mid')
